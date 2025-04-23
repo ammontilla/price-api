45bba4da9b417e47d7eecc072c728b3efe147798
Alvaro Montilla <amsystemdevelopment@gmail.com>
2025-04-23 13:43:27 +0200

    feat: inicializa proyecto Spring Boot con estructura base y dependencias

---
+++ src/test/java/com/inditex/price_api/PriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.price_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
